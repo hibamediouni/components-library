7b2018a2619508d48e1501a932c2ceeb9c0946d6
Hiba mediouni <hmediouni@cs.experts-comptables.org>
2020-01-29 11:08:32 +0100

    modif lib 3

--- src/lib/footer/footer.component.ts
+++ src/lib/footer/footer.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit() {
+    let user = "hibaaaa";
   }
 
 }
